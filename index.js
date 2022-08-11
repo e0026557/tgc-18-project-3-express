b3a577bd52bd23e35661336a6e61ab50de3b58de
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-11 05:30:05 +0000

    Reorganised codes in index.js

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ const csrf = require('csurf');
 require('dotenv').config();
 
 // *** SETUP ***
-const app = express(); // Create instance of Express app
-app.set('view engine', 'hbs'); // Set hbs as view engine
-app.set(express.static('public')); // Set up static folder
+// Create instance of Express app
+const app = express(); 
+
+// Set hbs as view engine
+app.set('view engine', 'hbs'); 
+
+// Set up static folder
+app.set(express.static('public')); 
 
 // Set up wax-on for template inheritance
 wax.on(hbs.handlebars);
@@ -24,23 +29,32 @@ wax.setLayoutPath('./views/layouts');
 const PORT = process.env.PORT || 3000;
 
 // *** GLOBAL MIDDLEWARES ***
-app.use(cors()); // Enable cross-site origin resource sharing
+// Enable cross-site origin resource sharing
+app.use(cors()); 
+
+// Enable form processing (required for CSRF token to work in forms)
 app.use(express.urlencoded({
   extended: false
-})); // Enable form processing (required for CSRF token to work in forms)
+})); 
+
+// Set up sessions
 app.use(session({
   store: new FileStore(), // use file to store sessions
   secret: process.env.SESSION_SECRET, // used to generate session id
   resave: false, // whether to recreate the session even if there is no change to it
   saveUninitialized: true // whether to create a new session when a new browser connects
-})); // Set up sessions
-app.use(flash()); // Set up flash messages (requires sessions)
+})); 
+
+// Set up flash messages (requires sessions)
+app.use(flash()); 
+
+// Share flash messages with hbs files
 app.use(function (req, res, next) {
   // res.locals will contain all the variables available to hbs files
   res.locals.success_messages = req.flash('success_messages');
   res.locals.error_messages = req.flash('error_messages');
   next();
-}); // Share flash messages with hbs files
+}); 
 
 //  Enable CSRF protection
 const csrfInstance = csrf();
@@ -54,19 +68,20 @@ app.use(function (req, res, next) {
   }
 });
 
+// Share CSRF with hbs files (must be included for all forms otherwise invalid CSRF error)
 app.use(function (req, res, next) {
   // Check if req.csrfToken is available
   if (req.csrfToken) {
     res.locals.csrfToken = req.csrfToken();
   }
   next();
-}); // Share CSRF with hbs files (must be included for all forms otherwise invalid CSRF error)
+}); 
 
-
+// Share user data across all hbs files
 app.use(function (req, res, next) {
   res.locals.user = req.session.user;
   next();
-}); // Share user data across all hbs files
+}); 
 
 
 app.get('/', function(req, res) {
@@ -74,5 +89,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(PORT, function() {
-  console.log('Server has started');
-});
+  console.log('Server has started')
+})
