731c3d5a9ac38c34d38a926c6d7b551f580043e4
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-13 09:53:16 +0000

    Added sample products object used for displaying products in dashboard

---
+++ sample.js
@@ -0,0 +1,21 @@
+let products = [
+  {
+    id: 2,
+    model: 'Studio',
+    weight: 310,
+    diameter: 121,
+    length: 126,
+    description: 'This LAMY studio fountain pen has a matte dark blue lacquer finish with high gloss chrome grip. It has a propeller shaped bright chrome metal clip and an interchangeable silver steel nib. This fountain pen comes with a blue LAMY ink cartridge and a Z27 converter for use with bottled ink so you can get started writing right away.',
+    brand_id: 1,
+    sale_status_id: 1,
+    cap_type_id: 5,
+    image_url: 'https://cdn.shopify.com/s/files/1/2603/2528/products/Lamy-Studio-ImperialBlue-NW-2_100x.jpg?v=1525274563',
+    thumbnail_url: 'https://cdn.shopify.com/s/files/1/2603/2528/products/Lamy-Studio-ImperialBlue-NW-2_100x.jpg?v=1525274563',
+    brand: { id: 1, brand: 'Pilot' },
+    capType: { id: 5, cap_type: 'Snap-cap' },
+    variants: [ [Object] ],
+    properties: [ [Object] ],
+    fillingMechanisms: [ [Object] ],
+    saleStatus: { id: 1, sale_status: 'Active' }
+  }
+];
