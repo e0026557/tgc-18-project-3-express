aa50200e39515d327aae0d59d2fb4cf48b4ea632
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-14 11:05:40 +0000

    Updated sample product

--- sample.js
+++ sample.js
@@ -21,7 +21,20 @@ let products = [
 ];
 
 let product = {
-  s
+  id: 2,
+  model: 'Studio',
+  weight: 31,
+  diameter: 12,
+  length: 126,
+  description: 'This LAMY studio fountain pen has a matte dark blue lacquer finish with high gloss chrome grip. It has a propeller shaped bright chrome metal clip and an interchangeable silver steel nib. This fountain pen comes with a blue LAMY ink cartridge and a Z27 converter for use with bottled ink so you can get started writing right away.',
+  brand_id: 2,
+  sale_status_id: 1,
+  cap_type_id: 5,
+  image_url: 'https://cdn.shopify.com/s/files/1/2603/2528/products/Lamy-Studio-ImperialBlue-NW-2_100x.jpg?v=1525274563',
+  thumbnail_url: 'https://cdn.shopify.com/s/files/1/2603/2528/products/Lamy-Studio-ImperialBlue-NW-2_100x.jpg?v=1525274563',
+  brand: { id: 2, brand: 'LAMY' },
+  capType: { id: 5, cap_type: 'Snap-cap' },
+  variants: [
     {
       id: 1,
       image_url: 'https://cdn.shopify.com/s/files/1/2603/2528/products/Lamy-Studio-ImperialBlue-NW-2_x67.jpg?v=1525274563',
