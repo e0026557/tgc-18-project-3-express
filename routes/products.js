b021221edc11b3bbc783ed43ed389d57db0dd9d7
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-14 04:53:12 +0000

    Added POST route for create products

--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const dataLayer = require('../dal/products');
 const { createProductForm, bootstrapField, createVariantForm } = require('../forms');
+const { FountainPen } = require('../models');
 
 // *** ROUTES ***
 
@@ -35,24 +36,56 @@ router.get('/', async function (req, res) {
 });
 
 router.get('/create', async function (req, res) {
-  const brands = await dataLayer.getAllBrands();
-  const capTypes = await dataLayer.getAllCapTypes();
-  const properties = await dataLayer.getAllProperties();
-  const fillingMechanisms = await dataLayer.getAllFillingMechanisms();
+  // Fetch all choices for product form
+  const choices = await dataLayer.getAllProductFormChoices();
 
-  const attributes = {
-    brands,
-    capTypes,
-    properties,
-    fillingMechanisms
-  };
-
-  const productForm = createProductForm(attributes);
+  const productForm = createProductForm(choices);
 
   res.render('products/create', {
     form: productForm.toHTML(bootstrapField),
   });
 })
+
+// TODO
+router.post('/create', async function (req, res) {
+  // Fetch all choices for product form
+  const choices = await dataLayer.getAllProductFormChoices();
+
+  const productForm = createProductForm(choices);
+
+  // Handle product form
+  productForm.handle(req, {
+    success: async function (form) {
+      // Extract product data from form data
+      const { properties, fillingMechanisms, ...productData } = form.data;
+
+      const product = new FountainPen(productData);
+      const newProduct = (await product.save()).toJSON();
+
+      // Attach m:m relationships
+      if (properties) {
+        await product.properties().attach(properties.split(','));
+      }
+
+      if (fillingMechanisms) {
+        await product.fillingMechanisms().attach(fillingMechanisms.split(','));
+      }
+
+      req.flash('success_messages', 'New product added successfully');
+      res.redirect('/products/newProduct.id/variants/create');
+    },
+    error: async function (form) {
+      res.render('products/create', {
+        form: form.toHTML(bootstrapField)
+      })
+    },
+    empty: async function (form) {
+      res.render('products/create', {
+        form: form.toHTML(bootstrapField)
+      })
+    }
+  })
+});
 
 // TODO
 router.get('/:product_id/variants', async function (req, res) {
