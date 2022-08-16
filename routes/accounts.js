ab246b17dddaf60fd4d767088b6c2b185c73c7a9
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-16 03:24:37 +0000

    Added GET and POST routes for registering user

--- routes/accounts.js
+++ routes/accounts.js
@@ -2,8 +2,52 @@
 const express = require('express');
 const router = express.Router();
 
+const dataLayer = require('../dal/accounts');
+const { createRegistrationForm, bootstrapField } = require('../forms');
+
 // *** ROUTES ***
-router.get('/login', function(req, res) {
+
+// TODO
+router.get('/register', function (req, res) {
+  const registerForm = createRegistrationForm();
+
+  res.render('accounts/register', {
+    form: registerForm.toHTML(bootstrapField)
+  });
+});
+
+router.post('/register', async function (req, res) {
+  const registerForm = createRegistrationForm();
+
+  // Process registration form
+  registerForm.handle(req, {
+    success: async function (form) {
+      // Create new user
+      try {
+        const user = await dataLayer.addUser(form.data);
+        req.flash('success_messages', 'Registered successfully');
+        res.redirect('/accounts/login');
+      } catch (error) {
+        console.log(error);
+        req.flash('error_messages', 'An error occurred while registering. Please try again');
+        res.redirect('/accounts/register');
+      }
+    },
+    error: function (form) {
+      res.render('accounts/register', {
+        form: form.toHTML(bootstrapField)
+      });
+    },
+    empty: function (form) {
+      res.render('accounts/register', {
+        form: form.toHTML(bootstrapField)
+      });
+    }
+  })
+});
+
+// TODO
+router.get('/login', function (req, res) {
   res.send('login page');
 })
 
