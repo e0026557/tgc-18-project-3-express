70113e466946639033e84b67fb72e51b7f34296b
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-16 04:11:32 +0000

    Formatted code

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,17 +1,16 @@
 const checkIfAuthenticated = function (req, res, next) {
-  if (req.session.user) {
-    next();
-  }
-  else {
-    // Store user's intended path before login
-    // req.originalUrl is the endpoint
-    req.session.redirectTo = req.originalUrl || '/';
+	if (req.session.user) {
+		next();
+	} else {
+		// Store user's intended path before login
+		// req.originalUrl is the endpoint
+		req.session.redirectTo = req.originalUrl || '/';
 
-    req.flash('error_messages', 'You need to log in to access this page');
-    res.redirect('/users/login');
-  }
-}
+		req.flash('error_messages', 'You need to log in to access this page');
+		res.redirect('/users/login');
+	}
+};
 
 module.exports = {
-  checkIfAuthenticated
-}
+	checkIfAuthenticated
+};
