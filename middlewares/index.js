9023f1e57176900bc230431b7948f3f864fc0f95
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-18 14:03:20 +0000

    Added checkIfAuthenticatedJWT middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const { sendResponse } = require("../utilities");
+
 const checkIfAuthenticated = function (req, res, next) {
 	if (req.session.user) {
 		next();
@@ -11,6 +14,38 @@ const checkIfAuthenticated = function (req, res, next) {
 	}
 };
 
+const checkIfAuthenticatedJWT = function (req, res, next) {
+	// Extract out the authorization headers
+	const authHeader = req.headers.authorization;
+	if (authHeader) {
+		// Extract out the JWT and check whether it is valid
+		// Example authHeader => BEARER ortsejlrtwlter
+		const token = authHeader.split(' ')[1];
+		jwt.verify(token, process.env.TOKEN_SECRET, function (err, tokenData) {
+			// err arg -> null if no error
+			// tokenData arg -> data that we embedded in JWT as payload
+			if (err) {
+				sendResponse(res, 401, {
+					error: 'Invalid access token'
+				});
+			}
+			else {
+				// If token is valid
+				req.user = tokenData; // so that the route can use this data
+				next();
+			}
+		})
+		next();
+	}
+	else {
+		sendResponse(res, 401, {
+			error: 'No authorization headers found'
+		});
+	}
+
+}
+
 module.exports = {
-	checkIfAuthenticated
+	checkIfAuthenticated,
+	checkIfAuthenticatedJWT
 };
