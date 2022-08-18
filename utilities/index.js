4fd643060c05f4309e6e2e4e46cd0a81fd9055c0
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-18 13:27:12 +0000

    Added validateEmail, sendResponse and sendDatabaseError functions

--- utilities/index.js
+++ utilities/index.js
@@ -8,6 +8,42 @@ const getHash = function (password) {
   return hash;
 }
 
+function validateEmail(email) {
+  let regex = /^[\w#][\w\.\’+#](.[\w\\’#]+)\@[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*(.[a-zA-Z]{2,20})$/;
+
+  if (email.match(regex)) {
+    return true;
+  }
+  return false;
+}
+
+function sendResponse (res, code, data) {
+  let status = '';
+  if ( String(code)[0] == '2') {
+    status = 'success';
+  }
+  else if (String(code)[0] == '4') {
+    status = 'fail';
+  }
+  
+  res.status(code);
+  res.json({
+    status: status,
+    data: data
+  });
+}
+
+function sendDatabaseError(res) {
+  res.status(500); // Internal server error
+  res.json({
+    status: 'error',
+    message: 'Unable to communicate with database'
+  });
+}
+
 module.exports = {
-  getHash
+  getHash,
+  validateEmail,
+  sendResponse,
+  sendDatabaseError
 }
