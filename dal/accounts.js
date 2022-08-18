bfbe51bd7f8ded4ea9212f44c9e1ea84049e9bfa
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-18 14:41:11 +0000

    Added addBlacklistedToken function

--- dal/accounts.js
+++ dal/accounts.js
@@ -4,45 +4,53 @@ const { getHash } = require('../utilities');
 
 // *** FUNCTIONS ***
 const addUser = async function (userData, roleId = 1) {
-  // Hash user's password
-  userData.password = getHash(userData.password);
+	// Hash user's password
+	userData.password = getHash(userData.password);
 
-  // Set user role to staff
-  userData.role_id = roleId;
+	// Set user role to staff
+	userData.role_id = roleId;
 
-  // Create new User instance with user data
-  const user = new User(userData);
-  await user.save();
+	// Create new User instance with user data
+	const user = new User(userData);
+	await user.save();
 
-  return user;
+	return user;
 };
 
 const getUserByCredentials = async function (formData) {
-  const user = await User.where({
-    username: formData.username,
-    password: getHash(formData.password)
-  }).fetch({
-    require: false,
-    withRelated: [
-      'role'
-    ]
-  });
+	const user = await User.where({
+		username: formData.username,
+		password: getHash(formData.password)
+	}).fetch({
+		require: false,
+		withRelated: ['role']
+	});
 
-  return user ? user : false; // Return user if exists, else false
-}
+	return user ? user : false; // Return user if exists, else false
+};
 
 const getBlacklistedToken = async function (refreshToken) {
-  const blacklistedToken = await BlacklistedToken.where({
-    token: refreshToken
-  }).fetch({
-    require: false
-  });
+	const blacklistedToken = await BlacklistedToken.where({
+		token: refreshToken
+	}).fetch({
+		require: false
+	});
 
-  return blacklistedToken;
-}
+	return blacklistedToken;
+};
+
+const addBlacklistedToken = async function (refreshToken) {
+	const token = new BlacklistedToken();
+	token.set('token', refreshToken);
+	token.set('date_created', new Date());
+	await token.save();
+
+	return token;
+};
 
 module.exports = {
-  addUser,
-  getUserByCredentials,
-  getBlacklistedToken
+	addUser,
+	getUserByCredentials,
+	getBlacklistedToken,
+	addBlacklistedToken
 };
