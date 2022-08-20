c42eb0640e64f32c0bb48a4115071e2f2102a448
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-20 03:08:51 +0000

    Added POST route for add to cart

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -8,16 +8,53 @@ const { sendResponse, sendDatabaseError } = require('../../utilities');
 
 router.get('/', async function (req, res) {
   const user = req.user; // From JWT token
-  // Get user's cart 
+  // Get user's cart
   try {
     const cartItems = await cartServices.getCart(user.id);
     sendResponse(res, 200, {
       cartItems: cartItems
-    })
+    });
   } catch (error) {
     console.log(error);
     sendDatabaseError(res);
   }
 });
+
+router.post('/:variant_id/add', async function (req, res) {
+  const userId = req.user.id;
+  const variantId = req.params.variant_id;
+  const quantity = req.body.quantity;
+
+  if (!userId || !variantId || !quantity) {
+    sendResponse(res, 400, {
+      error: 'Invalid parameter(s) specified'
+    });
+  }
+
+  // Add variant to user's cart
+  try {
+    const result = await cartServices.addToCart(
+      userId,
+      variantId,
+      quantity
+    );
+    if (result) {
+      sendResponse(res, 200, {
+        message: 'Item successfully added to cart'
+      });
+    } else {
+      sendResponse(res, 400, {
+        message: 'An error occurred while adding to cart'
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    sendDatabaseError(res);
+  }
+});
+
+// TODO: REMOVE FROM CART
+
+// TODO: UPDATE CART
 
 module.exports = router;
