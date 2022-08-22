3f39f3e63ec60108257541ca529cf137eaea0f5c
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-22 07:37:31 +0000

    Added orders page to view all orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,17 @@
 //  *** DEPENDENCIES ***
 const express = require('express');
 const router = express.Router();
+const dataLayer = require('../dal/orders');
 
 // *** ROUTES ***
+
+router.get('/', async function (req, res) {
+  // Get all orders
+  const orders = await dataLayer.getAllOrders();
+
+  res.render('orders/index', {
+    orders: orders.toJSON()
+  });
+});
 
 module.exports = router;
