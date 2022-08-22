0f643b1931fb2fceda546e9faf4fa76b021d52fb
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-22 03:40:41 +0000

    Edited addOrder function to remove redundant code

--- dal/orders.js
+++ dal/orders.js
@@ -6,15 +6,15 @@ const { Order, OrderItem } = require("../models");
 // Note: This is done inside charge succeeded event which does not contain the shipping option
 const addOrder = async function (orderData) {
   const order = new Order(orderData);
-  order.set('order_status_id', 3); // Set order status to paid
-  order.set('shipping_option', 'Normal Delivery'); // Set shipping option to be normal delivery (to be later updated by checkout session event which contains the actual selected shipping option)
-  order.set('order_date', new Date());
   await order.save();
 
   return order;
 }
 
 // TODO: CREATE NEW ORDER ITEM
+const addOrderItem = async function (orderItemData) {
+  const orderItem = new OrderItem(orderItemData);
+}
 
 // TODO: SEARCH ORDERS BY 
 // ANY 4:
