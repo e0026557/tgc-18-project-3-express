28d49d5c9e1d68b5d84607765e993692c97d217b
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-22 10:18:10 +0000

    Added GET route for all orders including search query

--- routes/orders.js
+++ routes/orders.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const dataLayer = require('../dal/orders');
+const { createOrderSearchForm, bootstrapField } = require('../forms');
 
 // *** ROUTES ***
 
@@ -9,8 +10,38 @@ router.get('/', async function (req, res) {
   // Get all orders
   const orders = await dataLayer.getAllOrders();
 
-  res.render('orders/index', {
-    orders: orders.toJSON()
+  // Get all fields required for order search form
+  const orderStatuses = await dataLayer.getAllOrderStatuses();
+  orderStatuses.unshift([0, '--- Any Order Status ---']);
+
+  const choices = {
+    orderStatuses
+  };
+
+  // Create search form for orders
+  const orderSearchForm = createOrderSearchForm(choices);
+
+  orderSearchForm.handle(req, {
+    success: async function (form) {
+      const orders = await dataLayer.filterOrdersBySearchFields(form);
+
+      res.render('orders/index', {
+        orders: orders.toJSON(),
+        form: form.toHTML(bootstrapField)
+      });
+    },
+    error: async function (form) {
+      res.render('orders/index', {
+        orders: orders.toJSON(),
+        form: form.toHTML(bootstrapField)
+      });
+    },
+    empty: async function (form) {
+      res.render('orders/index', {
+        orders: orders.toJSON(),
+        form: form.toHTML(bootstrapField)
+      });
+    }
   });
 });
 
