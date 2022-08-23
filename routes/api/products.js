8304b3fa7a9c3e5a6cdac9fcda75f44cdcd2bd2e
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-23 06:05:48 +0000

    Added GET route for retrieving a product and all its variants

--- routes/api/products.js
+++ routes/api/products.js
@@ -6,16 +6,29 @@ const { sendResponse, sendDatabaseError } = require('../../utilities');
 
 // *** ROUTES ***
 router.get('/', async function (req, res) {
-	// Retrieve all products
-	try {
-		const products = await dataLayer.getAllProducts();
-		sendResponse(res, 200, {
-			products
-		});
-	} catch (error) {
-		console.log(error);
-		sendDatabaseError(res);
-	}
+  // Retrieve all products
+  try {
+    const products = await dataLayer.getAllProducts();
+    sendResponse(res, 200, {
+      products
+    });
+  } catch (error) {
+    console.log(error);
+    sendDatabaseError(res);
+  }
+});
+
+router.get('/:product_id', async function (req, res) {
+  // Retrieve a product and its variants
+  try {
+    const product = await dataLayer.getProductById(req.params.product_id);
+    sendResponse(res, 200, {
+      product
+    })
+  } catch (error) {
+    console.log(error);
+    sendDatabaseError(res);
+  }
 });
 
 module.exports = router;
