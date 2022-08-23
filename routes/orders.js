1956f0e2b86a231a89bafe05d4e8eab7d643c774
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-23 03:34:50 +0000

    Added GET and POST routes for update orders

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const dataLayer = require('../dal/orders');
-const { createOrderSearchForm, bootstrapField } = require('../forms');
+const { createOrderSearchForm, bootstrapField, createUpdateOrderForm } = require('../forms');
 
 // *** ROUTES ***
 
@@ -44,5 +44,68 @@ router.get('/', async function (req, res) {
     }
   });
 });
+
+router.get('/:order_id/update', async function (req, res) {
+  // Get order to be updated
+  const order = await dataLayer.getOrderById(req.params.order_id);
+
+  // Fetch all choices for update order form
+  const orderStatuses = await dataLayer.getAllOrderStatuses();
+
+  // Create update order form and populate with existing data
+  const orderForm = createUpdateOrderForm({
+    orderStatuses
+  });
+
+  orderForm.fields.order_status_id.value = order.get('order_status_id');
+  orderForm.fields.order_date.value = order.get('order_date').toISOString().slice(0, 10);
+  if (order.get('delivery_date')) {
+    orderForm.fields.delivery_date.value = order.get('delivery_date');
+  }
+
+  res.render('orders/update', {
+    form: orderForm.toHTML(bootstrapField)
+  })
+});
+
+router.post('/:order_id/update', async function (req, res) {
+  // Get order to be updated
+  const orderId = req.params.order_id;
+  const order = await dataLayer.getOrderById(orderId);
+
+  // Fetch all choices for update order form
+  const orderStatuses = await dataLayer.getAllOrderStatuses();
+
+  // Process update order form
+  const orderForm = createUpdateOrderForm({
+    orderStatuses
+  });
+  orderForm.handle(req, {
+    success: async function (form) {
+      const { order_date, orderData } = form.data;
+
+      try {
+        await dataLayer.updateOrder(orderId, orderData);
+
+        req.flash('success_messages', 'Order successfully updated');
+        res.redirect('/orders');
+      } catch (error) {
+        console.log(error);
+        req.flash('error_messages', 'An error occurred while updating order. Please try again');
+        res.redirect(`/orders/${orderId}/update`);
+      }
+    },
+    error: async function (form) {
+      res.render('orders/update', {
+        form: form.toHTML(bootstrapField)
+      });
+    },
+    empty: async function (form) {
+      res.render('orders/update', {
+        form: form.toHTML(bootstrapField)
+      });
+    }
+  });
+})
 
 module.exports = router;
